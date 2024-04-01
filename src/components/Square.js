13d9999821d0fcee9d59182ce4d6d7ed71d7bc7f
mlaflam <mlaflam@users.noreply.github.com>
2024-04-01 15:27:38 -0400

    Adding handleClick in Square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
-const Square = ({ value }) => {
+const Square = () => {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+    //console.log("clicked!")
+  }
+
   return (
-    <button className="square">{value}</button>
+    <button
+      className="square"
+      onClick = {handleClick}
+    >
+      {value}
+    </button>
   )
 }
 
