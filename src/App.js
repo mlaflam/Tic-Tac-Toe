c5e6d3adf25695f4ee46ad3886d1df66bb62d075
mlaflam <mlaflam@users.noreply.github.com>
2024-04-01 22:32:18 -0400

    Initializing Game component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import Square from "./components/Square";
 import Board from "./components/Board";
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+     // TODO
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board/>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="get-info">
         <ol>{/*TODO*/}</ol>
